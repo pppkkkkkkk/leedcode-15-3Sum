a8422db825107c585a8609562cb720d64417eef5
Raymond Pang <124433610+pppkkkkkkk@users.noreply.github.com>
2025-09-17 21:20:16 +0000

    Remove the use of set

--- code.py
+++ code.py
@@ -5,18 +5,22 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        result = set()
+        result = []
         for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                i+=1
+                continue
             l = i+1
             r = len(nums)-1
             while r>l:
                 tmp = nums[i] + nums[l] + nums[r]
                 if (tmp == 0):
-                    result.add((nums[i],nums[l],nums[r]))
+                    result.append([nums[i],nums[l],nums[r]])
+                    while l < r and nums[l] == nums[l + 1]: l += 1
+                    while l < r and nums[r] == nums[r - 1]: r -= 1
+                    
                 if (tmp > 0):
                     r-=1
                 else:
                     l+=1
-
-        listResult = list(result)
-        return listResult 
+        return result 
